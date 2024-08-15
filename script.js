2b4e792a550236eb720f083122b2ffd2ef580eba
Samaelish <semnd007@gmail.com>
2024-08-15 21:12:15 +0300

    rewrote js code so that when content overlay is clicked the class 'open' is removed

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const openModalBtnElement = document.querySelector('#openModal')
 const modalElement = document.querySelector('.modal')
 const modalContentElement = modalElement.querySelector('.modal__content')
+const modalOverlayElement = modalElement.querySelector('.modal__overlay')
+
 
 openModalBtnElement.addEventListener('click', () => {
   modalElement.classList.add('open')
 })
 
-modalContentElement.addEventListener('click', () => {
+modalOverlayElement.addEventListener('click', () => {
   modalElement.classList.remove('open')
 })
